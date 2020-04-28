23a787a9189850e7e220d78785dd848492b02b87
Hyuneon <37572040+HyuneonY@users.noreply.github.com>
2020-04-28 14:35:13 +0900

    first commit

---
+++ Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private float x_pos;
+    private float y_pos;
+    private float moving_speed;
+    private float rot_speed;
+
+    public GameObject obj;
+
+
+    void Start()
+    {
+        this.moving_speed = 5.0f;
+        this.rot_speed = 40.0f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Translation();
+        //Rotation();
+        Shoot();
+    }
+
+    void Translation()
+    {
+        float keyHorizontal = Input.GetAxis("Horizontal");
+        float keyVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * this.moving_speed * Time.smoothDeltaTime * keyHorizontal, Space.World);
+        transform.Translate(Vector3.forward * this.moving_speed * Time.smoothDeltaTime * keyVertical, Space.World);
+
+    }
+
+    void Rotation()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        h = h * this.rot_speed * Time.deltaTime;
+        v = v * this.rot_speed * Time.deltaTime;
+
+
+        transform.Rotate(Vector3.back * h);
+        transform.Rotate(Vector3.right * v);
+    }
+
+    void Shoot()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("space pressed");
+            Instantiate(obj, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1), Quaternion.identity);
+        }
+    }
+}
