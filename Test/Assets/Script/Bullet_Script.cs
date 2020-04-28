59483bf7395a8012727fe36f8fd09062ae47a663
Hyuneon <37572040+HyuneonY@users.noreply.github.com>
2020-04-28 14:39:16 +0900

    second commit

---
+++ Test/Assets/Script/Bullet_Script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Script : MonoBehaviour
+{
+    private float z_pos;
+    public GameObject fx_obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        z_pos = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        z_pos = 0.1f;
+        transform.Translate(0.0f, 0.0f, z_pos);
+
+        Debug.Log(z_pos);
+
+        Destroy(gameObject, 3);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy_Tag")
+        {
+            Debug.Log("collision");
+            Destroy(gameObject, 0);
+            Destroy(other.gameObject, 0);
+            Instantiate(fx_obj, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        }
+    }
+}
---
+++ Test/Assets/Script/Manager_Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager_Script : MonoBehaviour
+{
+    public GameObject obj;
+    private int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int a;
+
+        ++counter;
+
+        a = counter % 100;
+        if(a==0)
+        {
+            for(int i=0; i<5;i++)
+            {
+                float randomX = Random.Range(-5f, 5f);
+
+                Instantiate(obj, new Vector3(randomX, 1.0f, 50.0f), Quaternion.identity);
+            }
+        }
+    }
+}
