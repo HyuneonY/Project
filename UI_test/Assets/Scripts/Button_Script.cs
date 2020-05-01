6af8943b6e7ae672a154a77403e4e6cbd08e6af0
Hyuneon <37572040+HyuneonY@users.noreply.github.com>
2020-05-01 16:51:07 +0900

    UI

---
+++ UI_test/Assets/Scripts/Button_Script.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Button_Script : MonoBehaviour
+{
+    [SerializeField] private InputField Name_Input;
+    [SerializeField] private InputField Password_Input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MyInputField_Name()
+    {
+        Debug.Log("이름을 입력했습니다.");
+        if(Name_Input.text == "YANG")
+        {
+            Debug.Log("존재하는 ID 입니다.");
+        }
+    }
+    public void MyInputField_Password()
+    {
+        Debug.Log("암호를 입력했습니다.");
+        if (Name_Input.text == "1234")
+        {
+            Debug.Log("암호가 일치합니다.");
+        }
+    }
+    public void MyButtonScript()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
---
+++ UI_test/Assets/Scripts/Sound_Script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sound_Script : MonoBehaviour
+{
+    private AudioSource music;
+    public Slider sound;
+    public static bool bCheck_Parameter = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.music = this.gameObject.GetComponent<AudioSource>();
+        this.music.loop = false;
+        music.volume = sound.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        music.volume = sound.value;
+    }
+
+    public void MyCheckButtonScript()
+    {
+        if (bCheck_Parameter == true)
+        {
+            sound.value = 0;
+            bCheck_Parameter = false;
+        }
+        else if (bCheck_Parameter == false)
+        {
+            sound.value = 1;
+            bCheck_Parameter = true;
+        }
+
+        Debug.Log("My check button:" + bCheck_Parameter);
+    }
+}
